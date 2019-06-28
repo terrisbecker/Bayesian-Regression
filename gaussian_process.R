e9e335f08ebf36a9eb05abb3977ddbccefeef122
TERRIS-PC\terri <terrisbecker@gmail.com>
2019-06-27 21:12:18 -0700

    Support for arbitrary mean and two covaraince functions

--- gaussian_process.R
+++ gaussian_process.R
@@ -1,11 +1,30 @@
 library(MASS)
-library(ridist)
+library(rdist)
+library(wesanderson)
 
-# My gaussian process function. Uses the gaussian kernel as a covariance matrix and has
-# mean of zero. Looking to support other mean and covariance functions soon. 
-gp <- function(x){
-	mean.vec <- integer(length(x))
-	cov.vec <- exp(-0.5*(pdist(x)/.4)^2)
+# Supports two covariance functions
+	# 'gaussian' -  uses the squared exponential kernel
+	# 'sigma' - uses the identity matrix multiplied by a scalar. i.e. the obs are inedpendent
+
+gp <- function(x, mean.vec, kernel = 'gaussian', sigma = 1){
+	if (kernel == 'gaussian'){
+		cov.vec <- exp(-0.5*pdist(x)^2)
+	}
+	if (kernel == 'sigma'){
+		cov.vec <- sigma*diag(length(x))
+	}
 	f <- mvrnorm(1, mean.vec, cov.vec)
 	return(f)
+}
+
+# An example
+x <- seq(1,10, by = 0.1)
+mean.vec <- integer(length(x))
+f <- gp(x, mean.vec)
+mycols <- wes_palette('Zissou1', 5, type = 'continuous')
+
+plot(x, f, ylim = c(-5,5), col = mycols[1], type = 'l')
+for (i in 2:5){
+	f <- gp(x, mean.vec)
+	lines(x, f, col = mycols[i])
 }
